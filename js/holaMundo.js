18efa6f8e1891488162a02bdbfa65ef984001ac4
Cristian <cbrandino@gmail.com>
2021-08-12 11:37:36 -0300

    primera practica de js

---
+++ js/holaMundo.js
@@ -0,0 +1,32 @@
+// Esto es un comentario de una sola linea
+
+/* Esto es un comentario
+ de 
+ multiples lineas*/
+
+// Mostrar un mensaje en consola. Punto y coma (;) buena praxis pero no obligatorio.
+ console.log("hola comision 16i");
+
+ //Mostrar un mensaje en html
+ document.write("Este es mi primer mensaje")
+
+ //Mostrar u mensaje en una ventana emergente. Comillas obligatorias para escribir texto.
+ alert("Hola Mundo");
+
+ // Crear una variable (var - let - const). Nombrar variables sin espacios; usar guio bajo o cammel case. Las comillas toman todo como texto.
+
+ let comision = "16i";
+
+// ir a otra linea con el texto: escribir <b>texto a imprimir: (con dos puntos finales). Dejar un espacio, cerrar comillas y escribir +nomre variable.
+ document.write("<br>comision: "+comision)
+
+ // Modificar el contenido de una variable.
+ comision = "i";
+ document.write("<br>comision "+comision)
+
+ const anio = 2021;
+ document.write("<br>Año "+anio)
+
+
+
+ 
