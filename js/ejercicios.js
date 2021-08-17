c38bbfd8e3d8ed66acc1f83b816c296dd289ab63
Cristian <cbrandino@gmail.com>
2021-08-17 09:46:04 -0300

    actualizadas lineas 6 y 7

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -3,6 +3,6 @@ document.write("Hello World");
 document.write(3+5);
 let nombre = prompt("NombreUsuario")
 document.write("<br>Hola "+nombre)
-var n1 = prompt("Primer numero")
-var n2 = prompt("Segundo numero")
+let n1 = prompt("Primer numero")
+let n2 = prompt("Segundo numero")
 document.write("suma "+ ( parseInt(n1)+ parseInt(n2)))
