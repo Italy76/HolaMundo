01671bea7ee2a099fd6928255312e53a506097f3
Cristian <cbrandino@gmail.com>
2021-08-17 10:57:45 -0300

    resolucion ejercicio 5

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -3,7 +3,7 @@ document.write("Hello World");
 document.write(3+5);
 let nombre = prompt("NombreUsuario")
 document.write("<br>Hola "+nombre)
-let numero1 = ParceInt (prompt("Ingrese primer numero"))
-let numero2 = ParceInt (prmpt("Ingrese segundo numero"))
-document.write ( "La suma es " + (numero1+numero2))
+let numero1 = parseInt(prompt("Ingrese primer numero"))
+let numero2 = parseInt(prompt("Ingrese segundo numero"))
+document.write ("La suma es "+ (numero1 + numero2))
 
