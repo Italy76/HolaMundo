8d431c3692ac9c3c4ddb57c0f48013aa4a54a7ae
Cristian <cbrandino@gmail.com>
2021-08-19 01:55:17 -0300

    ejercicios 6 y 7

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -3,7 +3,12 @@ document.write("Hello World");
 document.write(3+5);
 let nombre = prompt("NombreUsuario")
 document.write("<br>Hola "+nombre)
+/* parseInt("numero"). Transforma numero que esta en formato texto entre comillas, 
+en numero real para operaciones matematicas. esta funcion evita concatenar, porque 
+sabemos que si tenemos el signo de + entre numero y texto, no suma, sino que
+concatena.*/
 let numero1 = parseInt(prompt("Ingrese primer numero"))
 let numero2 = parseInt(prompt("Ingrese segundo numero"))
+//poner parentesis entre los dos numeros, sino, en presecia de un texto, los + concatenan.
+//El espacio en la ultima comilla sirve para dar espacio en eltexto que aparece en la pagina
 document.write ("La suma es "+ (numero1 + numero2))
-
--- js/estructuraCondicional.js
+++ js/estructuraCondicional.js
@@ -5,18 +5,37 @@ document.write ("<br> Numero 2= "+numero2);
 
 // Estructura if 
 /* if(condicion logica){
-codigo a ejecutar i la respuesta es verdadera o true
+codigo a ejecutar si la respuesta es verdadera o true
 }
 */
 
 if(numero1 > numero2){
     document.write ("<br> El numero 1 es mayor que el numero 2");
-}
-else if(numero1 == numero2){
-    document.write ("<br> El numero 2 es mayor que el numero 1"); 
+}else if(numero1 == numero2){
+    document.write("El numero 1 y el numero 2 son iguales")
 }
 else{
-  
-    //codigo a ejecutar cuando no se cumple la condicion
-    document.write ("<br> Los numeros ingresados son iguales");
+    //codigo a ejecutar si no se cumple la condicion
+    document.write("<br> El numero 2 es mayor que el numero 1")
+}
+
+//concole.log sirve para visualizar en la consola las variables y/o los resultados
+console.log('hola')
+console.log(numero1)
+
+//Ejercicio 6
+if(numero1 > numero2){
+    document.write("<br> El numero 1 es mayor del numero 2")
+}else{
+    document.write("<br> El numero 2 es mayor del numero 1")
+}
+//Ejercicio 7
+let numero3 = parseInt (prompt("Ingrese un tercer numero"));
+document.write ("<br> Numero 3= "+numero3);
+if(numero1 > numero2 && numero1 > numero3){
+    document.write("<br> El numero mayor es el numero 1")
+}else if(numero2 > numero1 && numero2 > numero3){
+    document.write ("<br> El numero mayor es el numero 2")
+}else {
+    document.write ("<br> El numero mayor es el numero 3")
 }
