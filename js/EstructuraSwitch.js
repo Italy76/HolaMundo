63198ec2457a6ba918eadcac517d87fda7357936
Cristian <cbrandino@gmail.com>
2021-08-23 02:13:42 -0300

    se agrego archivo de estruxtura switch con modificaciones a los comentarios

--- js/EstructuraSwitch.js
+++ js/EstructuraSwitch.js
@@ -1,16 +1,16 @@
 /* 
-switch(operacion){
+switch(variable){
     case 1:
         //todo el codigo necesario si la opcion que eligio el usuario es 1
         break;
         case 2:
-            // ...
+            // todo el codigo necesario si la opcion que eligio el usuario es 2
             break;
             case "otros"
-            ....;
+            //todo el codigo necesario si la opcion que eligio el usuario es otros
             break;
             default:
-                // codigo a ejecutarpor defecto
+                // codigo a ejecutar por defecto
 }
 
 */
@@ -32,5 +32,9 @@ switch(estacion){
             document.write("El usuario selecciono la estacion Primavera");
         break;
         default:
-            alert ("Ingreso un valor invalido");
+            alert ("Ingrese un valor invalido");
 }
+/* En este switch los casos contemplan texto. La variable es un prompt, por lo tanto da lugar 
+a una cadena de texto. El usuario debe selecconar entre numeros 1, 2, 3, 4, que son com texto. Por eso
+en los varios case los numros estan escritos con "" ("1", "2", "3", "4".)
+*/
