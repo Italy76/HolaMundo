d42b5d99664cc67af7d060a2ab85e6a7907aa16a
Cristian <cbrandino@gmail.com>
2021-08-26 08:37:36 -0300

    Hecho ejercicio 1 arreglos

---
+++ js/EjercicioArrays.js
@@ -0,0 +1,7 @@
+let meses = ["enero", "febrero","marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre",
+ "octubre", "noviembre", "diciembre"];
+ console.log(meses.length);
+
+ for(let position = 0; position < meses.length; position++){
+     document.write("<br>"+ meses[position])
+ };
